df681552242237da977a5c61201a2235d6274576
gvsmanohar <gedelavs.manohar@gmail.com>
2024-02-11 11:39:44 +0000

    added aws_security_group resource

--- us-east-1/main.tf
+++ us-east-1/main.tf
@@ -39,3 +39,32 @@ resource "aws_subnet" "subnet_3" {
     }
 }
 
+resource "aws_security_group" "SecurityGroup1" {
+  name        = "manu-security-group "
+  description = "Allow inbound traffic on specified ports"
+  vpc_id      = aws_vpc.vpc1.id
+  tags = {
+    Name = "manu-secutity-group"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
